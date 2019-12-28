9e8d8aa071ae0a95dd372f622a9566062f3ca7fb
Hanasou <royzhang42@gmail.com>
2019-12-27 23:45:54 -0800

    some classes

---
+++ app/src/main/java/com/example/a195a_e_senior_project/AdvisingHubActivity.java
@@ -0,0 +1,13 @@
+package com.example.a195a_e_senior_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdvisingHubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advising_hub);
+    }
+}
